8fe80892124926905b0e198730f8e5002f5da98e
Hosung Lim <ghtjd5087@gmail.com>
2025-11-01 12:57:31 +0900

    feat: fastapi init

--- nasa_mobile/lib/main.dart
+++ nasa_mobile/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NaSa Mobile',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
       home: const HomePage(),
     );
